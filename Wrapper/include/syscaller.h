5b5cb4e6ed85f6ab0f0f681a237b4927c206a294
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-06-01 16:22:46 -0700

    SysCaller

--- Wrapper/include/syscaller.h
+++ Wrapper/include/syscaller.h
@@ -1,4 +1,18 @@
 #pragma once
+/*
+ * SysCaller SDK
+ * Copyright (c) 2025 WindowsAPI
+ *
+ * License: GPLv3
+ *
+ * This software is free to use, modify, and distribute under the terms
+ * of the GNU General Public License version 3.
+ *
+ * You MAY NOT sell this software or derivative versions without also releasing
+ * their full source code under the same license.
+ *
+ * For more information, see https://www.gnu.org/licenses/gpl-3.0.html
+ */
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
