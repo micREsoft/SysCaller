6e6e81040d96be992f717fa4d140ef4398051fba
null <82195276+WindowsAPI@users.noreply.github.com>
2025-06-16 21:26:30 -0700

    __init__.py
    modularized main gui code

---
+++ BuildTools/GUI/components/__init__.py
@@ -0,0 +1,17 @@
+from .button import SysCallerButton
+from .output import SysCallerOutput
+from .progress_bar import SysCallerProgressBar
+from .status_bar import StatusBar
+from .title_bar import TitleBar
+from .left_panel import LeftPanel
+from .right_panel import RightPanel
+
+__all__ = [
+    'SysCallerButton',
+    'SysCallerOutput',
+    'SysCallerProgressBar',
+    'StatusBar',
+    'TitleBar',
+    'LeftPanel',
+    'RightPanel'
+]
