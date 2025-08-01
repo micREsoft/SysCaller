10fe8e1e5dc92b5962ff820520be90d6fd7376da
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-08-01 02:04:21 -0700

    Add ControlFlow.h to repo

---
+++ Bind/include/Core/Obfuscation/ControlFlow/ControlFlow.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QString>
+#include <QStringList>
+#include <QMap>
+#include <QSettings>
+#include <QSet>
+
+class ControlFlow {
+public:
+    explicit ControlFlow(QSettings* settings = nullptr);
+    QStringList generateOpaquePredicates(const QString& labelPrefix);
+    QStringList generateBogusControlFlow(const QString& labelPrefix);
+    QStringList generateIndirectJumps(const QString& labelPrefix);
+    QStringList generateConditionalBranches(const QString& labelPrefix);
+    void setSettings(QSettings* settings);
+    QString generateRandomLabel(const QString& prefix);
+    QStringList wrapWithControlFlow(const QStringList& originalCode, const QString& labelPrefix);
+    bool isOpaquePredicatesEnabled();
+    bool isBogusControlFlowEnabled();
+    bool isIndirectJumpsEnabled();
+    bool isConditionalBranchesEnabled();
+    int getControlFlowComplexity();
+
+private:
+    QSettings* settings;
+    QSet<QString> usedLabels;
+    int getRandomInt(int min, int max);
+    QString getRandomRegister();
+    QString getRandomCondition();
+    QStringList generateAlwaysTrueCondition();
+    QStringList generateAlwaysFalseCondition();
+    QStringList generateComplexPredicate();
+};
