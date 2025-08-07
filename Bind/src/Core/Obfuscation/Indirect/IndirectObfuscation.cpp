5ec45ac5437cf79614fb20e791ffd2b5efb742bc
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-08-07 00:33:46 -0700

    Update wtih safe junk, resolver call obfuscation, and id encryption
    - Add safe junk instructions, resolver call obfuscation, and id encryption

--- Bind/src/Core/Obfuscation/Indirect/IndirectObfuscation.cpp
+++ Bind/src/Core/Obfuscation/Indirect/IndirectObfuscation.cpp
@@ -100,8 +100,7 @@ bool IndirectObfuscation::processIndirectAssemblyFile(const QString& asmPath, co
             }
             if (inProcBlock) {
                 if (settings->value("obfuscation/indirect_enable_junk", true).toBool()) {
-                    JunkGenerator junkGen(settings);
-                    QString junkCode = junkGen.generateJunkInstructions();
+                    QString junkCode = generateRegisterSafeJunk();
                     if (!junkCode.isEmpty()) {
                         QStringList junkLines = junkCode.split('\n');
                         for (const QString& junkLine : junkLines) {
@@ -157,51 +156,119 @@ bool IndirectObfuscation::processIndirectAssemblyFile(const QString& asmPath, co
 }
 
 QString IndirectObfuscation::obfuscateResolverCall(const QString& originalCall) {
+    if (settings->value("obfuscation/indirect_obfuscate_calls", true).toBool()) {
+        QString method = settings->value("obfuscation/indirect_resolver_method", "random").toString();
+        if (method == "random") {
+            method = QString::number(QRandomGenerator::global()->bounded(4));
+        }
+        QMap<QString, int> methodMap;
+        methodMap["register"] = 0;
+        methodMap["stack"] = 1;
+        methodMap["indirect"] = 2;
+        methodMap["shuffle"] = 3;
+        int pattern = methodMap.value(method, 0);
+        switch (pattern) {
+            case 0:
+                return "    ; Register based function pointer obfuscation\n"
+                       "    lea r10, [GetSyscallNumber]\n"
+                       "    call r10";
+            case 1:
+                return "    ; Stack based function pointer with proper alignment\n"
+                       "    sub rsp, 16\n"                    
+                       "    lea rax, [GetSyscallNumber]\n"
+                       "    mov [rsp], rax\n"
+                       "    call qword ptr [rsp]\n"
+                       "    add rsp, 16";                     
+            case 2:
+                return "    ; Indirect call through data section\n"
+                       "    lea rax, [GetSyscallNumber]\n"
+                       "    mov [rsp-8], rax\n"
+                       "    lea rax, [rsp-8]\n"
+                       "    call qword ptr [rax]";
+            case 3:
+                return "    ; Register shuffle obfuscation\n"
+                       "    push r10\n"
+                       "    lea r10, [GetSyscallNumber]\n"
+                       "    call r10\n"
+                       "    pop r10";
+        }
+    }
     return originalCall;
 }
 
-QString IndirectObfuscation::generateObfuscatedResolver() {
-    QString resolver = "GetSyscallNumber PROC\n";
-    resolver += "    ; Obfuscated resolver implementation\n";
-    resolver += "    push rcx\n";
-    resolver += "    push rdx\n";
-    resolver += "    push r8\n";
-    resolver += "    push r9\n";
-    resolver += "    ; Add junk instructions\n";
-    resolver += "    xor rax, rax\n";
-    resolver += "    test rcx, rcx\n";
-    resolver += "    ; Actual resolver logic\n";
-    resolver += "    call ResolveSyscallNumber\n";
-    resolver += "    pop r9\n";
-    resolver += "    pop r8\n";
-    resolver += "    pop rdx\n";
-    resolver += "    pop rcx\n";
-    resolver += "    ret\n";
-    resolver += "GetSyscallNumber ENDP\n";
-    return resolver;
-}
-
-QString IndirectObfuscation::generateObfuscatedHashTable() {
-    return "HashTable PROC\n    ; Obfuscated hash table\n    ret\nHashTable ENDP\n";
-}
-
-QString IndirectObfuscation::generateObfuscatedFunctionPointers() {
-    return "FunctionPointers PROC\n    ; Obfuscated function pointers\n    ret\nFunctionPointers ENDP\n";
-}
-
-QString IndirectObfuscation::generateJunkCodeForIndirect() {
-    JunkGenerator junkGen(settings);
-    return junkGen.generateJunkInstructions();
-}
-
 QString IndirectObfuscation::generateEncryptedSyscallNumbers() {
-    return "EncryptedNumbers PROC\n    ; Encrypted syscall numbers\n    ret\nEncryptedNumbers ENDP\n";
+    QString encryptedCode;
+    int encryptionKey = QRandomGenerator::global()->bounded(1, 256);
+    encryptedCode = QString("    ; Encrypted syscall number handling\n"
+                           "    ; Key: 0x%1\n"
+                           "    mov rax, [rsp+%2]\n"  // get syscall number from stack
+                           "    xor rax, 0x%3\n"       // decrypt with key
+                           "    mov [rsp+%2], rax\n"   // store decrypted number back
+                           "    ; Continue with normal syscall\n")
+                           .arg(encryptionKey, 2, 16, QChar('0'))
+                           .arg(QRandomGenerator::global()->bounded(8, 32))
+                           .arg(encryptionKey, 2, 16, QChar('0'));
+    return encryptedCode;
 }
 
-QString IndirectObfuscation::obfuscateHashComputation(const QString& functionName) {
-    return QString("    ; Obfuscated hash computation for %1\n").arg(functionName);
-}
-
-QString IndirectObfuscation::obfuscateFunctionPointerLookup(const QString& functionName) {
-    return QString("    ; Obfuscated function pointer lookup for %1\n").arg(functionName);
+QString IndirectObfuscation::generateRegisterSafeJunk() {
+    // for indirect stubs we need to be careful about register usage:
+    // rcx, rdx, r8, r9 are function parameters dont touch those!
+    // rbx, rsi, rdi, r12 are used to save rcx, rdx, r8, r9 dont touch those!
+    // r10 is used for function pointer dont touch this!
+    // we can ONLY safely use r11, r13, r14, r15, rax
+    QStringList safeJunkInstructions = {
+        "    nop\n",
+        "    xchg r11, r11\n",
+        "    xchg r13, r13\n",
+        "    xchg r14, r14\n",
+        "    xchg r15, r15\n",
+        "    xchg rax, rax\n",
+        "    push r11\n    pop r11\n",
+        "    push r13\n    pop r13\n",
+        "    push r14\n    pop r14\n",
+        "    push r15\n    pop r15\n",
+        "    pushfq\n    popfq\n",
+        "    test r11, r11\n",
+        "    test r13, r13\n",
+        "    test r14, r14\n",
+        "    test r15, r15\n",
+        "    lea r11, [r11]\n",
+        "    lea r13, [r13]\n",
+        "    lea r14, [r14]\n",
+        "    lea r15, [r15]\n",
+        "    mov r11, r11\n",
+        "    mov r13, r13\n",
+        "    mov r14, r14\n",
+        "    mov r15, r15\n",
+        "    pause\n",
+        "    fnop\n",
+        "    cld\n",
+        "    clc\n",
+        "    stc\n    clc\n",
+        "    cmc\n    cmc\n",
+        "    xor r11d, 0\n",
+        "    and r13d, -1\n",
+        "    or r14d, 0\n",
+        "    add r15d, 0\n",
+        "    sub rax, 0\n",
+        "    db 66h\n    nop\n",
+        "    db 0Fh, 1Fh, 00h\n",
+        "    db 0Fh, 1Fh, 40h, 00h\n",
+        "    shl r11, 0\n",
+        "    shr r13, 0\n",
+        "    inc r14\n    dec r14\n",
+        "    inc r15\n    dec r15\n",
+        "    prefetchnta [rsp]\n",
+        "    sfence\n",
+        "    lfence\n",
+        "    mfence\n"
+    };
+    int numInstructions = QRandomGenerator::global()->bounded(1, 4);
+    QString junkCode;
+    for (int i = 0; i < numInstructions; ++i) {
+        int index = QRandomGenerator::global()->bounded(safeJunkInstructions.size());
+        junkCode += safeJunkInstructions[index];
+    }
+    return junkCode;
 }
