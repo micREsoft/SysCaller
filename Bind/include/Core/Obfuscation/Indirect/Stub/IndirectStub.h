020aad32950cc6376913197136a6adbff8259eed
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-08-11 13:39:21 -0700

    add IndirectStub.h to repo

---
+++ Bind/include/Core/Obfuscation/Indirect/Stub/IndirectStub.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QString>
+#include <QStringList>
+#include <QSettings>
+#include <functional>
+
+namespace IndirectObfuscation {
+
+    class Stub {
+    private:
+        QSettings* settings;
+
+    public:
+        explicit Stub(QSettings* settings);
+
+        QString obfuscateResolverCall(const QString& originalCall);
+        QString generateRegisterSafeJunk();
+    };
+}
