eaf57bf9b1f1e9ca20b9d169f6a6f8729fc3559e
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-08-11 21:10:46 -0700

    add IndirectJunkGenerator.h to repo

---
+++ Bind/include/Core/Obfuscation/Indirect/Stub/IndirectJunkGenerator.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <QString>
+#include <QStringList>
+#include <QSettings>
+
+namespace IndirectObfuscation {
+
+    class JunkGenerator {
+    private:
+        QSettings* settings;
+
+    public:
+        explicit JunkGenerator(QSettings* settings);
+
+        QString generateRegisterSafeJunk();
+    };
+}
