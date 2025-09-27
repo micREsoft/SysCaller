29fd63665aef8c54638a25f7f7714b6b44481a72
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-09-27 14:07:54 -0700

    improve format for resolver functions

--- SysCaller/Wrapper/src/Resolver/Resolver.cpp
+++ SysCaller/Wrapper/src/Resolver/Resolver.cpp
@@ -1,18 +1,19 @@
 #if defined(SYSCALLER_DIRECT)
-#pragma message("SysCaller: Building via DIRECT syscall mode!")
+#pragma message("SysCaller: Building via DIRECT syscall mode")
 #elif defined(SYSCALLER_INDIRECT)
-#pragma message("SysCaller: Building via INDIRECT syscall mode!")
+#pragma message("SysCaller: Building via INDIRECT syscall mode")
 #elif defined(SYSCALLER_INLINE)
-#pragma message("SysCaller: Building via INLINE ASM syscall mode!")
+#pragma message("SysCaller: Building via INLINE ASM syscall mode")
 #else
-#pragma message("SysCaller: No build mode specified, defaulting to DIRECT!")
+#pragma message("SysCaller: No build mode specified, defaulting to DIRECT")
 #endif
 
 #if defined(SYSCALLER_BINDINGS)
-#pragma message("SysCaller: Building with BINDINGS support! (DLL export)")
+#pragma message("SysCaller: Building with BINDINGS support (DLL export)")
 #endif
 
 #ifdef SYSCALLER_INDIRECT
+// Indirect syscall mode,include resolver implementation
 #include "../../include/Resolver/Resolver.h"
 #include <windows.h>
 #include <winternl.h>
@@ -25,99 +26,149 @@ static std::unordered_map<std::string, DWORD> syscallCache;
 static HMODULE ntdllHandle = NULL;
 static BOOL resolverInitialized = FALSE;
 
-HMODULE GetNtdllHandle() {
-    if (ntdllHandle == NULL) {
+HMODULE GetNtdllHandle()
+{
+    if (ntdllHandle == NULL)
+    {
         ntdllHandle = GetModuleHandleA("ntdll.dll");
-        if (ntdllHandle == NULL) {
+
+        if (ntdllHandle == NULL)
+        {
             ntdllHandle = LoadLibraryA("ntdll.dll");
         }
     }
+
     return ntdllHandle;
 }
 
-DWORD ExtractSyscallNumber(LPVOID functionAddress) {
-    if (functionAddress == NULL) {
+DWORD ExtractSyscallNumber(LPVOID functionAddress)
+{
+    if (functionAddress == NULL)
+    {
         return 0;
     }
+
     BYTE* bytes = (BYTE*)functionAddress;
-    for (int i = 0; i < 64; i++) {
-        if (bytes[i] == 0xB8) {
+
+    for (int i = 0; i < 64; i++)
+    {
+        if (bytes[i] == 0xB8)
+        {
             DWORD syscallNumber = *(DWORD*)(&bytes[i + 1]);
-            if (syscallNumber <= 0xFFFF) {
+
+            if (syscallNumber <= 0xFFFF)
+            {
                 return syscallNumber;
             }
         }
     }
+
     return 0;
 }
 
-std::unordered_map<std::string, DWORD> ExtractSyscallsFromDll() {
+std::unordered_map<std::string, DWORD> ExtractSyscallsFromDll()
+{
     std::unordered_map<std::string, DWORD> syscallNumbers;
     HMODULE hNtdll = GetNtdllHandle();
-    if (!hNtdll) {
+
+    if (!hNtdll)
+    {
         return syscallNumbers;
     }
+
     PIMAGE_DOS_HEADER dosHeader = (PIMAGE_DOS_HEADER)hNtdll;
-    if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+
+    if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE)
+    {
         return syscallNumbers;
     }
+
     PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)((BYTE*)hNtdll + dosHeader->e_lfanew);
-    if (ntHeaders->Signature != IMAGE_NT_SIGNATURE) {
+
+    if (ntHeaders->Signature != IMAGE_NT_SIGNATURE)
+    {
         return syscallNumbers;
     }
-    if (ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress == 0) {
+
+    if (ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress == 0)
+    {
         return syscallNumbers;
     }
-    PIMAGE_EXPORT_DIRECTORY exportDir = (PIMAGE_EXPORT_DIRECTORY)((BYTE*)hNtdll + 
+
+    PIMAGE_EXPORT_DIRECTORY exportDir = (PIMAGE_EXPORT_DIRECTORY)((BYTE*)hNtdll +
         ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+
     DWORD* functions = (DWORD*)((BYTE*)hNtdll + exportDir->AddressOfFunctions);
     DWORD* names = (DWORD*)((BYTE*)hNtdll + exportDir->AddressOfNames);
     WORD* ordinals = (WORD*)((BYTE*)hNtdll + exportDir->AddressOfNameOrdinals);
-    for (DWORD i = 0; i < exportDir->NumberOfNames; i++) {
+
+    for (DWORD i = 0; i < exportDir->NumberOfNames; i++)
+    {
         const char* funcName = (const char*)((BYTE*)hNtdll + names[i]);
-        if (strncmp(funcName, "Nt", 2) != 0 && strncmp(funcName, "Zw", 2) != 0) {
+
+        if (strncmp(funcName, "Nt", 2) != 0 && strncmp(funcName, "Zw", 2) != 0)
+        {
             continue;
         }
+
         DWORD funcRVA = functions[ordinals[i]];
-        LPVOID funcAddress = (LPVOID)((BYTE*)hNtdll + funcRVA);        
+        LPVOID funcAddress = (LPVOID)((BYTE*)hNtdll + funcRVA);
         DWORD syscallNumber = ExtractSyscallNumber(funcAddress);
-        if (syscallNumber > 0 && syscallNumber <= 0xFFFF) {
+
+        if (syscallNumber > 0 && syscallNumber <= 0xFFFF)
+        {
             syscallNumbers[funcName] = syscallNumber;
         }
     }
+
     return syscallNumbers;
 }
 
-BOOL InitializeResolver() {
-    if (resolverInitialized) {
+BOOL InitializeResolver()
+{
+    if (resolverInitialized)
+    {
         return TRUE;
     }
+
     syscallCache = ExtractSyscallsFromDll();
-    if (syscallCache.empty()) {
+
+    if (syscallCache.empty())
+    {
         return FALSE;
     }
+
     resolverInitialized = TRUE;
     return TRUE;
 }
 
-DWORD GetSyscallNumber(const char* functionName) {
-    if (!resolverInitialized) {
-        if (!InitializeResolver()) {
+DWORD GetSyscallNumber(const char* functionName)
+{
+    if (!resolverInitialized)
+    {
+        if (!InitializeResolver())
+        {
             return 0;
         }
     }
+
     auto it = syscallCache.find(functionName);
-    if (it != syscallCache.end()) {
+
+    if (it != syscallCache.end())
+    {
         return it->second;
     }
+
     return 0;
 }
 
-void CleanupResolver() {
+void CleanupResolver()
+{
     syscallCache.clear();
     resolverInitialized = FALSE;
 }
 
 #else
+// Not in indirect mode file compiles to nothing
 #pragma message("SysCaller: Resolver.cpp skipped (SYSCALLER_INDIRECT not defined)")
 #endif
