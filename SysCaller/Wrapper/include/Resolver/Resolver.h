2a530763883313b6356c06ece71594e7e57d215e
WindowsAPI <contact@reverseengineeri.ng>
2025-10-20 16:04:59 -0700

    add WIN32_LEAN_AND_MEAN and NOMINMAX to includes
    This improves compilation times and avoids issues with min/max macros.

--- SysCaller/Wrapper/include/Resolver/Resolver.h
+++ SysCaller/Wrapper/include/Resolver/Resolver.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#include <windows.h>
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+#include <Windows.h>
 #include <winternl.h>
 
 #ifdef __cplusplus
