a7b2c3432aab101b06131027a26d7012d5fb2d36
WindowsAPI <82195276+WindowsAPI@users.noreply.github.com>
2025-11-11 23:01:08 -0800

    add missing main.cpp

---
+++ Bind/main.cpp
@@ -0,0 +1,33 @@
+#include <Core/Utils/Common.h>
+#include <GUI/MainWindow.h>
+
+int main(int argc, char *argv[]) {
+    qputenv("QT_LOGGING_RULES", "*.debug=true;qt.qpa.*=false");
+    QApplication app(argc, argv);
+    app.setStyle(QStyleFactory::create("Fusion"));
+    app.setWindowIcon(QIcon(":/Icons/logo.ico"));
+    int fontId = QFontDatabase::addApplicationFont(":/Fonts/ibmplexmono.ttf");
+    if (fontId != -1) {
+        QStringList fontFamilies = QFontDatabase::applicationFontFamilies(fontId);
+        if (!fontFamilies.isEmpty()) {
+            app.setFont(QFont(fontFamilies.first(), 10));
+        }
+    }
+    app.setStyleSheet(
+        "* {"
+        " font-family: 'IBM Plex Mono';"
+        "}"
+        "QToolTip {"
+        " background-color: #1E1E1E;"
+        " color: white;"
+        " border: 1px solid #2196F3;"
+        " border-radius: 4px;"
+        " padding: 5px;"
+        " font-family: 'IBM Plex Mono';"
+        "}"
+    );
+    QString projectRoot = PathUtils::getProjectRoot();
+    MainWindow w;
+    w.show();
+    return app.exec();
+}
